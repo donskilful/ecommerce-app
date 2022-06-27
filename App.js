a3958b5e60d6c6bfcc90e983a775f367a20b3ba3
donskilful <donskilful@gmail.com>
2022-06-27 09:21:08 +0100

    added react native swiper dependency

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from './src/components/Layout/Header'
+import HomeScreen from './src/screens/HomeScreen'
 
 const App = () => {
   return (
     <View>
-      <Header/>
+      <Header />
+      <HomeScreen/>
     </View>
   )
 }
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import React from 'react'
+import Swiper from 'react-native-swiper'
+
+const {width} = Dimensions.get('window');
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
